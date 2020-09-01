ac93cab25818bf5208a9c08dc1c279726919cb03
andrewhemans <andrewhemans@gmail.com>
2020-08-31 18:12:57 -0700

    added font, updated images

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`average`, `prata\:400,700`],
+        fonts: [`inter\:400,500,700,800`],
       },
     },
     {
